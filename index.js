7ca5468dee07f3cfa0982c326a3aa6d6b41ff33e
Roman Shtylman <shtylman@gmail.com>
2012-12-30 12:42:08 -0500

    support reading options from mocha.opts
    - test/mocha.opts will be read if available
    - you can also specify a directory of tests now
    
    closes #1

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ var optimist = require('optimist');
 // location of mocha resources
 var mocha_path = path.dirname(require.resolve('mocha'));
 
+var cwd = process.cwd();
+
 // html harness
 var index = fs.readFileSync(__dirname + '/fixtures/index.html', 'utf-8');
 
 var argv = optimist
-    .usage('zuul [options] js-test-file')
+    .usage('zuul [options] file(s)|dir')
     .describe('server', 'port to start harness server for manual testing')
     .describe('ui', 'mocha ui (bdd, tdd, qunit, exports')
-    .default('ui', 'bdd')
+    .default('ui')
     .argv;
 
 if (argv.help) {
@@ -34,17 +36,70 @@ if (argv.server && isNaN(parseInt(argv.server))) {
     process.exit(-1);
 }
 
+// if specified directory, then load mocha opts
+
 // bundle the javascript we are interested in
 var bundle = browserify({
     debug: true,
     watch: true
 });
-argv._.forEach(bundle.addEntry.bind(bundle));
+
+// user can specify files or directories
+// directories are checked for .js files
+argv._.forEach(function(file_or_dir) {
+    var stat = fs.statSync(file_or_dir);
+
+    if (stat.isFile()) {
+        return bundle.addEntry(file_or_dir);
+    }
+
+    // ignore non js and hidden files
+    var files = fs.readdirSync(file_or_dir).filter(function(file) {
+        return path.extname(file) === '.js' && file[0] !== '.';
+    });
+
+    files = files.map(function(file) {
+        return path.join(cwd, file_or_dir, file);
+    });
+
+    files.forEach(bundle.addEntry.bind(bundle));
+});
+
+// options which will be passed to `mocha.setup`
+var mocha_opt = {};
+
+// is there a mocha.opts file?
+var mocha_opts_path = path.join(cwd, 'test', 'mocha.opts');
+if (fs.existsSync(mocha_opts_path)) {
+    var content = fs.readFileSync(mocha_opts_path, 'utf8');
+
+    content.split('\n').forEach(function(line) {
+        if (line.length === 0) {
+            return;
+        }
+
+        var split = line.split(' ');
+
+        var key = split.shift().replace('--', '');
+        var value = split.join(' ');
+
+        mocha_opt[key] = value;
+    });
+}
+
+// backwards compat for command line arg
+// overrides anything in mocha.opt file
+if (argv.ui) {
+    mocha_opt.ui = argv.ui;
+}
+
+// the default (html) reporter must be used for browser testing
+delete mocha_opt.reporter;
 
 // setup http server to serve our harness files
 var app = express();
 app.get('/', function(req, res) {
-    res.send(index.replace('__mocha_ui__', argv.ui));
+    res.send(index.replace('__mocha_opts__', JSON.stringify(mocha_opt)));
 });
 app.get('/build.js', function(req, res) {
     res.contentType('application/javascript');
@@ -69,6 +124,17 @@ if (argv.server) {
 else {
     // location of mocha_phantomjs runner
     var mocha_phantom = require.resolve('mocha-phantomjs');
+    var phantom = require.resolve('phantomjs');
+
+    if (!mocha_phantom) {
+        console.error('mocha-phantomjs is not installed');
+        console.error('run: npm install -g mocha-phantomjs');
+        process.exit(-1);
+    }
+    else if (!phantom) {
+        console.error('phantomjs is not installed');
+        console.error('run: npm install -g phantomjs');
+    }
 
     // launch phantomjs to run the harness files
     var cmd = 'phantomjs';
