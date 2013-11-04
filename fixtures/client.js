1c35586e36507d14d4241e7c72ab37d36ca84def
Roman Shtylman <shtylman@gmail.com>
2013-11-03 21:42:36 -0500

    inital refactor to support running in the cloud

---
+++ fixtures/client.js
@@ -0,0 +1,28 @@
+var harness = window.mochaPhantomJS || mocha;
+if (harness.checkLeaks) {
+  harness.checkLeaks();
+}
+
+var runner = harness.run();
+
+// Listen to `runner` events to populate a global
+// `.mochaResults` var which may be used by selenium
+// to report on results.
+
+var failed = [];
+
+runner.on('fail', function(test, err){
+  failed.push({
+    title: test.title,
+    fullTitle: test.fullTitle(),
+    error: {
+      message: err.message,
+      stack: err.stack
+    }
+  });
+});
+
+runner.on('end', function(){
+  runner.stats.failed = failed;
+  global.mochaResults = runner.stats;
+});
