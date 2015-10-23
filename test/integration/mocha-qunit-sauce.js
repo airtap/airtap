ce74cd696e24fbd96bafa1c32b67509cb235a8a5
vvo <vincent.voyer@algolia.com>
2015-10-23 14:01:52 +0200

    test(ci): use browzers package for working list of browsers to test
    + make 5 the concurrency

--- test/integration/mocha-qunit-sauce.js
+++ test/integration/mocha-qunit-sauce.js
@@ -12,37 +12,20 @@ test('mocha-qunit - sauce', function(done) {
         prj_dir: __dirname + '/../fixtures/mocha-qunit',
         files: [__dirname + '/../fixtures/mocha-qunit/test.js'],
         username: auth.username,
-        concurrency: 1,
+        concurrency: 5,
         key: auth.key
     };
 
     var zuul = Zuul(config);
 
-    scout_browser(function(err, browsers) {
+    scout_browser(function(err, allBrowsers) {
         assert.ifError(err);
 
-        var total = 0;
-        Object.keys(browsers).forEach(function(key) {
-            var list = browsers[key];
-            if (list.length === 1) {
-                total++;
-                return zuul.browser(list);
-            }
+        var flattenBrowser = require('../../lib/flatten_browser');
+        var browsers = flattenBrowser(require('browzers').pullRequest, allBrowsers);
+        var total = browsers.length;
 
-            list = list.filter(function(browser) {
-                // iphone 9.1 buggy in saucelabs, never really start (Oct 2015)
-                return browser.name !== 'iphone' && browser.name !== 'ipad' || browser.version !== '9.1'
-            });
-
-            list.sort(function(a, b) {
-                return a.version - b.version;
-            });
-
-            // test latest and oldest
-            total += 2;
-            zuul.browser(list.shift());
-            zuul.browser(list.pop());
-        });
+        browsers.forEach(zuul.browser.bind(zuul));
 
         // N times per browser and once for all done
         done = after(total * 2 + 1, done);
