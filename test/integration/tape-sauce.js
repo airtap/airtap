a546c0ccd7af99d493a414acb582d14d665adb0a
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-03-18 13:03:20 +0100

    move auth.js into tape-sauce.js

--- test/integration/tape-sauce.js
+++ test/integration/tape-sauce.js
@@ -1,12 +1,15 @@
 var test = require('tape')
 var path = require('path')
+var os = require('os')
+var fs = require('fs')
+var yaml = require('yamljs')
 var sauceBrowsers = require('sauce-browsers/callback')
 var Zuul = require('../../')
-var auth = require('../auth')
 var browsersToTest = require('airtap-browsers').pullRequest
 var verify = require('./verify-common')
 
 test('tape - sauce', function (t) {
+  var auth = getAuth()
   var config = {
     prj_dir: path.resolve(__dirname, '../fixtures/tape'),
     files: [ path.resolve(__dirname, '../fixtures/tape/test.js') ],
@@ -35,3 +38,18 @@ test('tape - sauce', function (t) {
     verify(t, zuul)
   })
 })
+
+function getAuth () {
+  // optional additional config from $HOME/.airtaprc
+  var config = path.join(os.homedir(), '.airtaprc')
+  var rc = {}
+
+  if (fs.existsSync(config)) {
+    rc = yaml.parse(fs.readFileSync(config, 'utf-8'))
+  }
+
+  return {
+    username: process.env.SAUCE_USERNAME || rc.sauce_username,
+    key: process.env.SAUCE_ACCESS_KEY || rc.sauce_key
+  }
+}
