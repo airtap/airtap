bff9a3f35a6f637835f66419275d95391360e933
Vincent Weevers <mail@vincentweevers.nl>
2018-03-24 10:55:39 +0100

    fix cli test on windows

--- test/unit/airtap.js
+++ test/unit/airtap.js
@@ -8,7 +8,7 @@ test('exits cleanly and does nothing if no secure travis env', t => {
   const env = Object.assign({}, process.env, {
     TRAVIS_SECURE_ENV_VARS: 'false'
   })
-  exec(airtap, { env }, (err, stdout, stderr) => {
+  exec('node ' + airtap, { env }, (err, stdout, stderr) => {
     t.error(err, 'no error')
     t.equal(stdout.trim(), messages.SKIPPING_AIRTAP)
     t.end()
@@ -19,7 +19,7 @@ test('exits with error if no files are specified', t => {
   const env = Object.assign({}, process.env)
   // Delete this to be sure it's undefined
   delete env.TRAVIS_SECURE_ENV_VARS
-  exec(airtap, { env }, (err, stdout, stderr) => {
+  exec('node ' + airtap, { env }, (err, stdout, stderr) => {
     t.ok(err, 'should error')
     t.equal(stderr.trim(), messages.NO_FILES)
     t.end()
