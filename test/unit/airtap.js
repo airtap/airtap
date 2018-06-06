4b391e8f4422d149e3062866e00eaba86d68cc9c
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-06-06 13:18:39 +0200

    Add failing test for deduped browsers

--- test/unit/airtap.js
+++ test/unit/airtap.js
@@ -15,11 +15,35 @@ test('capabilities config', function (t) {
   var zuul = Zuul(config)
 
   zuul.browser({
-    name: 'internet explorer',
+    browser: 'internet explorer',
     version: '11'
   })
 
   var browser = zuul._browsers[0]
   t.same(browser._conf.capabilities, config.capabilities)
+  t.end()
+})
+
+test('browsers are deduped', function (t) {
+  var zuul = Zuul({})
+
+  zuul.browser({
+    browser: 'iphone',
+    version: '11.3',
+    platform: 'Mac 10.13'
+  })
+  zuul.browser({
+    browser: 'iphone',
+    version: '11.3',
+    platform: 'Mac 10.13'
+  })
+
+  var browsers = zuul._browsers
+
+  t.is(browsers.length, 1, 'should be deduped')
+  t.is(browsers[0]._conf.browser, 'iphone', '.browser correct')
+  t.is(browsers[0]._conf.version, '11.3', '.version correct')
+  t.is(browsers[0]._conf.platform, 'Mac 10.13', '.platform correct')
+
   t.end()
 })
